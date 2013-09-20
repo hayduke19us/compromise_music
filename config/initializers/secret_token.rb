b34da7bc9e576064f4d3917f284973fa09012ebe
hayduke19us <hayduke19us@gmail.com>
2013-09-20 00:28:43 -0500

    devise authentication is working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompromiseMusic::Application.config.secret_key_base = '7852315c3a9eff7439aea3207d144f007b30abf4d3ab176fd657efc657f41e0a292f92dd137bdd03c0dffe1f8be8ada5545db1f123a73528d850f4e3ce2b570e'
