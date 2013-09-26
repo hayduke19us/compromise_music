eb2b5f1973d809fdd9c74e608d6bb8f8d659f903
hayduke19us <hayduke19us@gmail.com>
2013-09-26 11:08:49 -0500

    image from oauth info  added to DB. Made excessible in sessions/index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130926154315) do
+ActiveRecord::Schema.define(version: 20130926160313) do
 
   create_table "users", force: true do |t|
     t.string   "provider"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20130926154315) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "oauth_expires_at"
+    t.binary   "image"
   end
 
 end
