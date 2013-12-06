4feab8597aa98cc8205ca12e347f96cb2ad2b0ba
hayduke19us <hayduke19us@gmail.com>
2013-12-06 14:30:51 -0600

    dependent delete for grouplist in playlist model fixe

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,5 @@
 class Playlist < ActiveRecord::Base
+  has_many :grouplists, :dependent => :destroy 
   has_many :tracks, :dependent => :destroy
   belongs_to :user
   validates :key, uniqueness: true
