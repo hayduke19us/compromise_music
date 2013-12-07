f1990a5302da3d4a9538ba8721bc644241c3488f
hayduke19us <hayduke19us@gmail.com>
2013-12-06 18:13:50 -0600

    adding groupships controller

---
+++ app/controllers/groupships_controller.rb
@@ -0,0 +1,9 @@
+class GroupshipsController < ApplicationController
+  def create
+    groupship = Groupship.new(group_id: params[:group_id], 
+                              friend_id: params[:friend_id])
+    if groupship.save
+      redirect_to root_path 
+    end
+  end
+end
