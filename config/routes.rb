56b2b5743f2ac22acb9fd5e3ced133b63d0b3c6f
hayduke19us <hayduke19us@gmail.com>
2013-12-08 16:44:13 -0600

    got the rdio playback player up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CompromiseMusic::Application.routes.draw do
  
+  get "helps/index"
   get "friends/show"
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
