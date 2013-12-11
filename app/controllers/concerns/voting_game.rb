59f55e988be9e8f02d8c369c12319c3575740604
hayduke19us <hayduke19us@gmail.com>
2013-12-10 21:05:47 -0600

    started working on game mech but I'm going to add a couple migrations. I'm going to add a 'points' model to that is owned by users user has one points model. in the model wil be various integer datatypes like simple_success

--- app/controllers/concerns/voting_game.rb
+++ app/controllers/concerns/voting_game.rb
@@ -1,11 +1,10 @@
 module Voting_Game
    def self.track_success_filter(playlist, group)
     success_tracks = []
-    failure_tracks = []
     friends = group.friends.count
     track_count = playlist.tracks.count
     playlist.tracks.each do |track|
-      fair_voting = (friends/track_count - 1)
+      fair_voting = (friends/track_count)
       unless track.votes_for >= fair_voting 
         failure_tracks << track
         track.destroy
@@ -13,7 +12,19 @@ module Voting_Game
         success_tracks << track
       end  
     end
-    
-    success_tracks.size - failure_tracks.size
+    for track in success_tracks
+      user =  User.find(track.user_id) 
+      simple_success(user)
+    end
   end
+   
+  private
+  
+  #tracks didn't get deleted when the playlist was published
+  #the lowest form of success
+  
+  def simple_success(user)
+    user.simple_success = user.simple_success + 1 
+    luck_dj.save 
+  end 
 end
