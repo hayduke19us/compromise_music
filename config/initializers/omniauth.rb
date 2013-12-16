dc098ee49a00e87d4193054eb4521925d4649bd5
hayduke19us <hayduke19us@gmail.com>
2013-12-16 17:58:10 -0600

    trying to get omniauth to work with heroku. stopped using faraday

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :rdio, ENV["RDIO_CONSUMER_KEY"], ENV["RDIO_CONSUMER_SECRET"]
+  provider :rdio, "vc76wjg3xyyqawc7m7dttjmq","NxMYesjNWW"
 end
