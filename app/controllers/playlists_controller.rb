3b2dc68f15e6a66a3d169b857f9dbc6aef7888ed
hayduke19us <hayduke19us@gmail.com>
2013-12-16 22:33:03 -0600

    deleted everything in playlist controller except for def new. and deleted everything in playlist view

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,69 +1,6 @@
 class PlaylistsController < ApplicationController
-  before_filter :get_rdio_user
   
-  def index
-  
-  end
-
   def new
-
   end
 
-  def create
-    unless params[:name].blank? || params[:description].blank?
-      RdioPlaylist.new_playlist(params[:name], params[:description])
-      playlist_params = RdioPlaylist.playlist_attributes(current_user.id)
-      playlist = Playlist.new(playlist_params)
-      playlist.save
-      flash[:notice] = "You have succesfully created a Playlist"
-      redirect_to user_playlist_path(playlist.user_id, playlist.id)
-    else
-        flash[:notice] = "A playlist requires a name and description"
-        redirect_to new_playlist_path
-    end
-  end
-
-  def show
-    @user = current_user
-    @playlist = Playlist.find(params[:id])
-    if params[:group]
-      @group_id = params[:group]
-    end
-    unless params[:play_track].blank?
-     @play_key = params[:play_track]
-    end
-    search_helper
-  end
-
-  def search_helper
-    if params[:query]
-      rdio_search =  RdioSearch.new(search_type: params[:search_type],
-                                      query: params[:query])
-      @search_results = rdio_search.simple
-    elsif params[:list]
-      @album_tracks = RdioSearch.new(list: params[:list]).album_tracks
-    elsif params[:artist_key]
-      rdio_search = RdioSearch.new(artist_key: params[:artist_key])
-      @search_results = rdio_search.artist_albums
-    end
-  end
-
-  def destroy
-    playlist = Playlist.find(params[:id])
-    RdioPlaylist.delete_playlist(playlist.key)
-    playlist.destroy
-    redirect_to root_url
-  end
-
-  def publish
-    playlist = Playlist.find(params[:id])
-    group = Group.find(params[:group])
-    if Grouplist.where(group_id: group.id,
-                       playlist_id: playlist.id)
-      result =  VotingGame.track_success_filter(playlist,
-                                               group,
-                                               playlist.tracks.count)
-      redirect_to user_playlist_path(playlist.user_id, playlist.id, group: group.id)
-    end
-  end
 end
