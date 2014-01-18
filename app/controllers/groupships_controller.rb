36b3187d79de203b6da7f82b2fa7beb0980890f5
hayduke19us <hayduke19us@gmail.com>
2014-01-18 01:10:03 -0600

    added groupships create ajax method

--- app/controllers/groupships_controller.rb
+++ app/controllers/groupships_controller.rb
@@ -1,10 +1,11 @@
 class GroupshipsController < ApplicationController
   respond_to :html, :js
   def create
+    @user = current_user
     groupship = Groupship.new(group_id: params[:group_id], 
                               friend_id: params[:friend_id])
     if groupship.save
-      redirect_to root_path 
+      respond_with @user
     else
       redirect_to root_path
     end
