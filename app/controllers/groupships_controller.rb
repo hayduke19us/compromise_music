e0d43561b9cdb1fa0a5b5256d2cca76341318ca1
hayduke19us <hayduke19us@gmail.com>
2014-01-18 23:02:11 -0600

    create groupship bug fixed I need to put except_friends method into users model

--- app/controllers/groupships_controller.rb
+++ app/controllers/groupships_controller.rb
@@ -1,15 +1,32 @@
 class GroupshipsController < ApplicationController
   respond_to :html, :js
   def create
-    @user = current_user
+    @users = users_except_friends
     groupship = Groupship.new(group_id: params[:group_id], 
                               friend_id: params[:friend_id])
     if groupship.save
-      respond_with @user
+      respond_with @users
     else
       redirect_to root_path
     end
   end
+
+  def users_except_friends
+    unless current_user.friends.blank?
+      all_users = User.where.not("id IN (#{except_array})")
+    else
+      all_users = User.where("id != ?", current_user.id)  
+    end
+    @all_users = all_users
+  end
+  def except_array
+    arr = []
+    arr << current_user.id
+    current_user.friends.each {|f| arr << f.id}
+    arr.join(",")
+  end
+
+
 
   def destroy
     @group = Group.find(params[:group_id])
