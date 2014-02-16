4925d60b7b28f8cc99513c6cf477e04985337d18
hayduke19us <hayduke19us@gmail.com>
2014-02-16 01:42:54 -0600

    cleaned up tree

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-
 require 'rails/all'
 
 # Pick the frameworks you want:
@@ -25,10 +24,10 @@ module CompromiseMusic
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # 
-    #config for heroku assets precompiled locally 
+    #
+    #config for heroku assets precompiled locally
     config.assets.initialize_on_precompile = false
-    
+
     config.generators do |g|
       g.test_framework :mini_test, :spec => false, :fixture => true
     end
