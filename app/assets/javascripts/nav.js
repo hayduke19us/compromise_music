d51230fe6888b628481d2e4a46858c0d3cbf7d75
hayduke19us <hayduke19us@gmail.com>
2014-02-19 13:33:36 -0600

    header nav fixed using JS instead of HTML

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,6 +1,15 @@
-$( document ).ready (function nav(){
-  if ($("#current_page").length > 0){  
-    var doc = document.getElementById("current_page").className;
-    document.getElementById(doc).className="active";
-  }
+$(function () {
+
+  $("#playlist_pg").click(function () {
+    $("#analytics_pg").attr("class", "");
+    $("#home_pg").attr("class", "");
+    $("#playlist_pg").attr("class", "active");
+  });
+
+  $("#analytics_pg").click(function () {
+    $("#playlist_pg").attr("class", "");
+    $("#home_pg").attr("class", "");
+    $("#analytics_pg").attr("class", "active");
+  });
+
 });
