6da55017d7e264bb284c2a0955ace277e627940a
hayduke19us <hayduke19us@gmail.com>
2014-02-25 17:19:07 -0600

    added playlist tagging capabilities...tags all tracks in playlist

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,16 +2,28 @@ class TagsController < ApplicationController
   respond_to :js
 
   def create
-    track = Track.find(params[:taggable_id].to_i)
     tag = Tag.new(name: params[:name],
                   taggable_id: params[:taggable_id],
                   taggable_type: params[:taggable_type])
-    playlist = Playlist.find(track.playlist_id)
-    if tag.save
-      tag.tagged_playlist
-      sync_update track
-      @response = response_hash(playlist) 
-      respond_with @response 
+
+    if params[:taggable_type] == 'Track'
+      track = Track.find(params[:taggable_id].to_i)
+      playlist = Playlist.find(track.playlist_id)
+      if tag.save
+        sync_update track
+        @response = response_hash(playlist) 
+        respond_with @response 
+      else
+        redirect_to root_url
+      end
+    elsif params[:taggable_type] == 'Playlist'
+      playlist = Playlist.find(params[:taggable_id].to_i)
+      if tag.save
+        tag.tagged_playlist
+        playlist.tracks.each { |track| sync_update track} 
+        @response = response_hash(playlist) 
+        respond_with @response 
+      end
     else
       redirect_to root_url
     end
